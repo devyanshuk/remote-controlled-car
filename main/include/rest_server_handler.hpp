4da4ece9babb9a2b97400c23dadd60d1932735fd
devyanshuk <devkoirala10@gmail.com>
2021-08-26 03:40:05 +0200

    proper spacing

--- main/include/rest_server_handler.hpp
+++ main/include/rest_server_handler.hpp
@@ -10,12 +10,12 @@
 #include "constants.h"
 
 
-void            register_callbacks(httpd_handle_t * server);
-esp_err_t       root_handler(httpd_req_t *);
-esp_err_t       pwm_handler(httpd_req_t *);
-esp_err_t       stop_handler(httpd_req_t *);
-esp_err_t       left_handler(httpd_req_t *);
-esp_err_t       right_handler(httpd_req_t *);
-esp_err_t       forward_handler(httpd_req_t *);
-esp_err_t       backward_handler(httpd_req_t *);
-esp_err_t       set_content_type_from_file(httpd_req_t *, std::string);
+void            register_callbacks          (httpd_handle_t * server);
+esp_err_t       root_handler                (httpd_req_t *);
+esp_err_t       pwm_handler                 (httpd_req_t *);
+esp_err_t       stop_handler                (httpd_req_t *);
+esp_err_t       left_handler                (httpd_req_t *);
+esp_err_t       right_handler               (httpd_req_t *);
+esp_err_t       forward_handler             (httpd_req_t *);
+esp_err_t       backward_handler            (httpd_req_t *);
+esp_err_t       set_content_type_from_file  (httpd_req_t *, std::string);
